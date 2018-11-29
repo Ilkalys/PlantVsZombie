adaf3fb2e9854a6aabbf7b6a08915dd796e02d1a
Matthieu <matthieu.gauget.berlioz@gmail.com>
2018-11-29 12:51:50 +0100

    Update ZombieSpawner, à regler

--- PlantsVsZombies/src/Resources/ZombieSpawner.java
+++ PlantsVsZombies/src/Resources/ZombieSpawner.java
@@ -1,22 +1,28 @@
 package Resources;
 
+
+import java.util.PriorityQueue;
 import java.util.Random;
 
 import Resources.GameWorld;
 
 public class ZombieSpawner {
 
+	private PriorityQueue<Integer> listeZombies = new PriorityQueue<Integer>();
 	private Random rand = new Random();
 	private Timer timer;
 	
 	ZombieSpawner(int difficulty){
 		this.timer = new Timer(20000);
+		for(int i=0; i< 20 ; i++ ) {
+			listeZombies.add(rand.nextInt(10));
+		}
 	}
 	
 	public void step() {
-		if(timer.hasFinished()) {
+		if(timer.hasFinished() && !listeZombies.isEmpty()) {
 				GameWorld.addZombie(1.2, (double)rand.nextInt(5)/10 +0.3, false);
-				timer = new Timer(10000);
+				timer = new Timer(listeZombies.poll()*2000);
 		}
 	}
 	
