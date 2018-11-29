3e58d572c637f6f11f103180fb379fa0a0291d69
Matthieu <matthieu.gauget.berlioz@gmail.com>
2018-11-29 22:06:02 +0100

    Sprite Maj

--- PlantsVsZombies/src/Screens/MenuStart.java
+++ PlantsVsZombies/src/Screens/MenuStart.java
@@ -2,44 +2,57 @@ package Screens;
 
 import Resources.Game;
 import Resources.StdDraw;
+import Resources.Timer;
 
 public class MenuStart extends GameScreen {
-	
+
 	private double ecart;
+	private boolean start;
+	private Timer fondu;
 
 	public MenuStart() {
 		ecart = 0;
 	}
-	
+
 	@Override
-	public void processUserInput(char key) {
-		// TODO Auto-generated method stub
-		
+	public void processUserInput(char key) {		
 	}
 
 	public void processMouseClick(double x, double y) {
 		if(x <= 0.589 && x >= 0.429 && y >= 0.62 && y <= 0.68) {
-			Game.setWorld(new MenuLevelComplete());
+			start = true;
 		}
 	}
 
 	public void step() {	
-		ecart += 0.001;
-		if (ecart >= 1.2)
-			ecart = -0.2;
+		if (!start) {
+			ecart += 0.001;
+			if (ecart >= 1.2)
+				ecart = -0.2;
+		}
+		else {
+			if(fondu == null)
+				fondu = new Timer(2000);
+			else if(fondu.hasFinished())
+				Game.setWorld(new GameWorld());
+		}
+			
+			
 	}
 
 	@Override
 	public void dessine() {
-		
+
 		StdDraw.setFont();
-		StdDraw.picture(0.5, 0.5,"/Pictures/Accueil1.png", 1, 1);
+		StdDraw.picture(0.5, 0.5,(start)?"/Pictures/Accueil2.png":"/Pictures/Accueil1.png", 1, 1);
 		StdDraw.picture(0+ecart, 0.8,"/Pictures/CloudDown.png", 0.51, 0.51);
 		StdDraw.picture(1-ecart, 0.97,"/Pictures/CouldUp.png", 0.51, 0.51);
 		StdDraw.picture(0.509, 0.57,"/Pictures/Affichage.png", 0.51, 0.51);
 		StdDraw.text(0.509, 0.65,"Nouvelle Partie");
+		if(start)
+			StdDraw.picture(0.5, 0.5, "/Pictures/Fondu.png",5,5);
 
-		
+
 	}
 
 }
