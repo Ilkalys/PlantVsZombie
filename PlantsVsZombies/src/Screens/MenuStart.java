104e662c222063f4b5ce14b32415209d099738a3
Matthieu <Matthieu@192.168.0.40>
2018-12-13 12:16:27 +0100

    JavaDocFait

--- PlantsVsZombies/src/Screens/MenuStart.java
+++ PlantsVsZombies/src/Screens/MenuStart.java
@@ -20,45 +20,74 @@ public class MenuStart extends GameScreen {
 	 */
 	//------------------------------------------------------------------------------
 
-	private double ecart;
-	private boolean start;
-	private Timer fondu;
-	private File SpriteFilepath;
-	private boolean menuSetButton = false;
-	private boolean menuInfos = false;
-	private char changeButton;
+	// Chemin vers les dossiers de sprites
+	private File SpriteFilepath  = new File("sprites");
 
+	// Verifie si le bouton pour commencer à été pressé
+	private boolean launchGame;
+	// Timer Avant le lancement du jeu une fois le bouton pressé
+	private Timer launchTimer;
+
+	// Veut-on afficher le menu de configuration de touche?
+	private boolean menuSetButton = false;
+	// Récupère la touche que l'on veut changer
+	private char changeButton = ' ';
+	// Veut-on afficher le menu d'Infos?
+	private boolean menuInfos = false;
+
+	// Gère la position en X des Nuages
+	private double posXCloud = 0;
+
+	//------------------------------------------------------------------------------
+	/*
+	 **      CONSTRUCTEUR
+	 */
+	//------------------------------------------------------------------------------
 
 	public MenuStart() {
+		//Démarrage de la musique si celle-ci n'a pas déjà demarrée Via une autre scène
 		if(SoundPlayer.getBGMPlayed() != "Blues.wav")
 			SoundPlayer.PlayBGM("Blues.wav");
-		this.ecart = 0;
-		SpriteFilepath = new File("sprites");
+	}
+
+	//------------------------------------------------------------------------------
+	/*
+	 **      METHODES PUBLIQUES
+	 */
+	//------------------------------------------------------------------------------
+
+	/**  
+	 * Gestion des interactions clavier avec l'utilisateur, ici Uniquement lors du changement des touches
+	 *	
+	 * @param key Touche pressee par l'utilisateur
+	 */
+	@Override
+	public void processUserInput(char key) {	
+		//Verifie si la touche appuyée n'est pas déjà prise.
+		if(key != Sunflower.getKey() && key != PeasShooter.getKey() && key != Nuts.getKey() && key != Dynamite.getKey() && key != 'i') {
+			//Vérifie quelle touche l'utilisateur veut changer
+			if(changeButton == Sunflower.getKey()) {
+				Sunflower.setKey(key);
+			} else if(changeButton == PeasShooter.getKey()) {
+				PeasShooter.setKey(key);
+			} else if(changeButton == Nuts.getKey()) {
+				Nuts.setKey(key);
+			} else if(changeButton == Dynamite.getKey()) {
+				Dynamite.setKey(key);
+			}
+		}
+		//Remet changeButton de façon à ne selectionner aucune touche
 		changeButton = ' ';
 	}
 
-	@Override
-	public void processUserInput(char key) {	
-		if(changeButton == Sunflower.getKey()) {
-			if(key != PeasShooter.getKey() && key != Nuts.getKey() && key != Dynamite.getKey() && key != 'i')
-				Sunflower.setKey(key);
-			changeButton = ' ';
-		} else if(changeButton == PeasShooter.getKey()) {
-			if(key != Sunflower.getKey() && key != Nuts.getKey() && key != Dynamite.getKey() && key != 'i')
-				PeasShooter.setKey(key);
-			changeButton = ' ';
-		} else if(changeButton == Nuts.getKey()) {
-			if(key != PeasShooter.getKey() && key != Sunflower.getKey() && key != Dynamite.getKey() && key != 'i')
-				Nuts.setKey(key);
-			changeButton = ' ';
-		} else if(changeButton == Dynamite.getKey()) {
-			if(key != PeasShooter.getKey() && key != Nuts.getKey() && key != Sunflower.getKey() && key != 'i')
-				Dynamite.setKey(key);
-			changeButton = ' ';
-		}
-	}
-
+	/**
+	 * Gestion des interactions souris avec l'utilisateur (la souris a ete clique)
+	 * 
+	 * @param x position en x de la souris au moment du clic
+	 * @param y position en y de la souris au moment du clic
+	 */
 	public void processMouseClick(double x, double y) {
+		//Gestion des interactions souris lorsqu'on est dans le Menu de Configuration de Touches : Gère le bouton Retour et quel touche veut être changer celon où l'on clique
 		if(menuSetButton) {
 			if(x <= 0.6  && x >= 0.3 && y >= 0.13 && y <= 0.18) {
 				SoundPlayer.PlaySE("fire.wav");
@@ -82,29 +111,36 @@ public class MenuStart extends GameScreen {
 				changeButton = Dynamite.getKey();
 			}
 		}
+		//Gestion des interactions souris lorsqu'on est dans le Menu d'Informations : gère uniquement le bouton retour
 		else if(menuInfos) {
 			if(x <= 0.6  && x >= 0.3 && y >= 0.10 && y <= 0.15) {
 				SoundPlayer.PlaySE("fire.wav");
 				menuInfos = false;
 			}
 		}
+		// Gestion des interactions souris sur la Scène Initale
 		else {
+			// Lance le jeu après un delai
 			if(x <= 0.589 && x >= 0.429 && y >= 0.62 && y <= 0.68) {
 				SoundPlayer.PlaySE("fire.wav");
-				start = true;
+				launchGame = true;
 			}
+			// Ouvre le menu de selection de niveau
 			else if(x <= 0.589 && x >= 0.429 && y >= 0.54 && y <= 0.59) {
 				SoundPlayer.PlaySE("fire.wav");
 				Game.setWorld(new MenuSelectionLevel());
 			}
+			// Ferme le Jeu
 			else if(x <= 0.589 && x >= 0.429 && y >= 0.02 && y <= 0.07) {
 				SoundPlayer.PlaySE("fire.wav");
 				Game.setStopGame(true);
 			}
+			// Ouvre le Menu de Configuration de Touche
 			else if(x <= 0.16 && x >= 0.04 && y >= 0.015 && y <= 0.065) {
 				SoundPlayer.PlaySE("fire.wav");
 				menuSetButton = true;
 			}
+			// Ouvre le Menu d'Informations
 			else if(x <= 0.96 && x >= 0.84 && y >= 0.015 && y <= 0.065) {
 				SoundPlayer.PlaySE("fire.wav");
 				menuInfos = true;
@@ -113,60 +149,84 @@ public class MenuStart extends GameScreen {
 
 	}
 
+	/**
+	 * Fait bouger/agir toutes les entites
+	 */
 	public void step() {	
-		if (!start) {
-			ecart += 0.001;
-			if (ecart >= 1.2)
-				ecart = -0.2;
+		//Gestion du déplacement des Nuages tant que l'on a pas cliqué sur le bouton demarré
+		if (!launchGame) {
+			posXCloud += 0.001;
+			if (posXCloud >= 1.2)
+				posXCloud = -0.2;
 		}
+		//Gestion de LaunchTimer si l'utilisateur veut demarré le jeu
 		else {
-			if(fondu == null)
-				fondu = new Timer(2000);
-			else if(fondu.hasFinished())
+			if(launchTimer == null)
+				launchTimer = new Timer(2000);
+			else if(launchTimer.hasFinished())
 				Game.setWorld(new GameWorld(1));
 		}
 
 
 	}
 
+	/**
+	 * Dessine les entites du jeu
+	 */
 	@Override
 	public void dessine() {
 
 		StdDraw.setFont();
 		StdDraw.setPenColor(StdDraw.BLACK);
-		StdDraw.setFont();
-		StdDraw.picture(0.5, 0.5,(start)?SpriteFilepath.getAbsolutePath() + "/bg/Accueil2.png":SpriteFilepath.getAbsolutePath() + "/bg/Accueil1.png", 1, 1);
-		StdDraw.picture(0+ecart, 0.8,SpriteFilepath.getAbsolutePath() + "/set/CloudDown.png", 0.51, 0.51);
-		StdDraw.picture(1-ecart, 0.97,SpriteFilepath.getAbsolutePath() + "/set/CouldUp.png", 0.51, 0.51);
+		
+		//Gère le Fond, le Titre et les Nuages
+		StdDraw.picture(0.5, 0.5,(launchGame)?SpriteFilepath.getAbsolutePath() + "/bg/Accueil2.png":SpriteFilepath.getAbsolutePath() + "/bg/Accueil1.png", 1, 1);
+		StdDraw.picture(0+posXCloud, 0.8,SpriteFilepath.getAbsolutePath() + "/set/CloudDown.png", 0.51, 0.51);
+		StdDraw.picture(1-posXCloud, 0.97,SpriteFilepath.getAbsolutePath() + "/set/CouldUp.png", 0.51, 0.51);
 		StdDraw.picture(0.509, 0.57,SpriteFilepath.getAbsolutePath() + "/set/Affichage.png", 0.51, 0.51);
 		StdDraw.picture(0.5, 0.9, SpriteFilepath.getAbsolutePath() + "/set/title.png");
+		
+		//Affichage du Texte
 		StdDraw.setFont(new Font("sans serif",10,20));
 		StdDraw.text(0.509, 0.65,"Nouvelle Partie");
 		StdDraw.text(0.509, 0.57,"Choix Level");
 		StdDraw.text(0.509, 0.05,"Quitter le Jeu");
-
+		
+		//Affichage des Boutons du Menu de Configuration de Touches et du Menu d'Informations
 		StdDraw.picture(0.1, 0.05,SpriteFilepath.getAbsolutePath() + "/button/keyboard.png",0.06,0.06);
 		StdDraw.picture(0.1, 0.05, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.06,0.04);
 		StdDraw.picture(0.9, 0.05,SpriteFilepath.getAbsolutePath() + "/button/Interrogation.png",0.03,0.03);
 		StdDraw.picture(0.9, 0.05, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.06,0.04);
 
+		//Affichage du Menu de Configuration de Touches si demandé
 		if(menuSetButton)
 			menuSetButton();
+		//Affichage du Menu d'Informations si demandé
 		else if(menuInfos)
 			menuInfos();
-		else if(start)
+		//Effet Assombrissant lors du démarrage du jeu
+		else if(launchGame)
 			StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",5,5);
-
-
 	}
 
+	//------------------------------------------------------------------------------
+	/*
+	 **      METHODES PRIVEES
+	 */
+	//------------------------------------------------------------------------------
+	/**
+	 * Gère l'affichage du Menu de Configuration de Touches
+	 */
 	private void menuSetButton() {
 		StdDraw.setPenColor(StdDraw.WHITE);
 		StdDraw.setFont(new Font("sans serif",15,40));
 
+		// Affichage du Fond
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
+
+		//Affichage du Texte, les couleurs variants en fonction de quel bouton va être changé
 		StdDraw.text(0.5, 0.85,"Changement de touches :");
 
 		StdDraw.setPenColor((changeButton == Sunflower.getKey())?StdDraw.RED : StdDraw.WHITE);
@@ -181,23 +241,29 @@ public class MenuStart extends GameScreen {
 		StdDraw.setPenColor((changeButton == Dynamite.getKey())?StdDraw.RED : StdDraw.WHITE);
 		StdDraw.text(0.5, 0.3,"Spawn Dynamite :" + Dynamite.getKey());
 
+		// Affichage du Bouton de Retour
 		StdDraw.setPenColor(StdDraw.WHITE);
 		StdDraw.text(0.5, 0.15,"Retour");
+
+		//Affichage d'un Texte dans une autre police
 		StdDraw.setFont(new Font("sans serif",15,20));
 		StdDraw.text(0.5, 0.7,"Touche i = Infos");
-
-		StdDraw.setPenColor(StdDraw.BLACK);
 	}
 
+	/**
+	 * Gère l'affichage du Menu d'Informations
+	 */
 	private void menuInfos() {
 		StdDraw.setPenColor(StdDraw.WHITE);
 		StdDraw.setFont(new Font("sans serif",15,40));
 
+		// Affichage du Fond
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 		StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",0.8,0.8);
 
+		// Affichage du Texte
 		StdDraw.text(0.5, 0.85,"Informations");
 		StdDraw.setFont(new Font("sans serif",15,20));
 		StdDraw.text(0.5, 0.78,"Etoiles : Monnaie du jeu, apparaît aléatoirement");
@@ -217,7 +283,7 @@ public class MenuStart extends GameScreen {
 
 
 		StdDraw.setFont(new Font("sans serif",15,30));
-
+		//Affichage du Bouton Retour
 		StdDraw.text(0.5, 0.12,"Retour");
 	}
 }
--- PlantsVsZombies/src/Screens/MenuVictory.java
+++ PlantsVsZombies/src/Screens/MenuVictory.java
@@ -16,45 +16,91 @@ public class MenuVictory extends GameScreen {
 	 */
 	//------------------------------------------------------------------------------
 
-	private File SpriteFilepath;
+	// Chemin vers les dossiers de sprites
+	private File SpriteFilepath = new File("sprites");
+
+	// Durée de la musique de victoire
 	private double durationSong = 22.857;
+	// Timer determinant quand la musique se lance, 
+	//puis une fois lancé devient un Timer de la durée de la musique pour gerer les animations
 	private Timer launchSong;
+	// indique où en est la musique
 	private double actualTime;
-	// L'effet sonore a-t'il ete joue ou non
-	private boolean sePlayded;
+
+	// Position Y initial du backGround
 	private double bgYPosition = 0.25;
+	// Position initial des personnages qui dansent
 	private double persoYPosition;
+
+	// L'effet sonore a-t'il été joué ou non
+	private boolean sePlayded = false;
+	// La musique a-t'elle été joué ou non
 	private boolean alreadyPlay = false;
 
+	//------------------------------------------------------------------------------
+	/*
+	 **      CONSTRUCTEUR
+	 */
+	//------------------------------------------------------------------------------
 
+	/**
+	 * Construit la scène de Victoire
+	 */
 	public MenuVictory() {
+		// créer un délai avant le démarrage de la chanson
 		launchSong = new Timer((int)(4_000));
-		SpriteFilepath = new File("sprites");
-		this.sePlayded = false;
+		// arrête la chanson qui était joué précédement (s'il y en avait une)
 		SoundPlayer.StopBGM();
 	}
 
+
+	//------------------------------------------------------------------------------
+	/*
+	 **      METHODES
+	 */
+	//------------------------------------------------------------------------------
+
+	/**  
+	 * Gestion des interactions clavier avec l'utilisateur
+	 *	
+	 * @param key Touche pressee par l'utilisateur
+	 */
 	@Override
 	public void processUserInput(char key) {
+		// il n'y a rien à faire avec des touches.
 	}
 
+	/**
+	 * Gestion des interactions souris avec l'utilisateur (la souris a ete clique)
+	 * 
+	 * @param x position en x de la souris au moment du clic
+	 * @param y position en y de la souris au moment du clic
+	 */
 	@Override
 	public void processMouseClick(double x, double y) {
+		// si la musique a été joué une fois, le joueur peut retourner au menu principal en cliquant n'importe où.
 		if(alreadyPlay ) {
 			SoundPlayer.PlaySE("fire.wav");
 			Game.setWorld(new MenuStart());
 		}
 	}
 
+	/**
+	 * Fait bouger/agir toutes les entites
+	 */
 	@Override
 	public void step() {	
+		// initialise la position des personnages sur la scène
 		persoYPosition = bgYPosition + 0.13;
+		// lorsque le Timer est finis alors que la musique est arretée, la musique et le Timer démarrent
+		//Le Timer prend comme durée le temps de la musique
 		if(SoundPlayer.getBGMClip() == null) {
 			if(launchSong.hasFinished()) {
 				SoundPlayer.PlayBGM("Win.wav");
 				launchSong = new Timer((int)(durationSong * 1_000));
 			}
 		}
+		// Lorsque la musique tourne, calcul de actualTime et lorsque la musique et le Timer se termine on les relance en précisant alreadyPlay
 		else {
 			actualTime = durationSong - launchSong.getActualTime();
 			if(launchSong.hasFinished()) {
@@ -62,21 +108,23 @@ public class MenuVictory extends GameScreen {
 				launchSong = new Timer((int)(durationSong * 1_000));
 			}
 		}
-
+		// Active le bruitage du spot lumineux si celui-ci n'a pas déja été fait lorsqu'il reste 0.5s au Timer
 		if(launchSong.getActualTime() <= 0.5 && !this.sePlayded) {
 			SoundPlayer.PlaySE("spot.wav");
 			this.sePlayded = true;
 		}
 	}
 
+	/**
+	 * Dessine les entites du jeu
+	 */
 	@Override
 	public void dessine() {
-		//fond
-		StdDraw.setPenColor(StdDraw.BLACK);
+		//Affichage du BackGround, en fonction de bgYPosition
 		StdDraw.picture(0.5, bgYPosition, SpriteFilepath.getAbsolutePath() + "/bg/MenuVictory.png", 1, 1.5);
-		//affichage texte
-		StdDraw.setPenColor(StdDraw.WHITE);
 
+		//affichage du texte et le cercle où sont les danseurs lorsque la musique démarre 
+		StdDraw.setPenColor(StdDraw.WHITE);
 		if(SoundPlayer.getBGMClip() != null){
 			StdDraw.setFont(new Font("sans serif",10,50));
 			StdDraw.text(0.5, 0.97, "Félicitations!");
@@ -85,6 +133,8 @@ public class MenuVictory extends GameScreen {
 			StdDraw.picture(0.5, persoYPosition - 0.18, SpriteFilepath.getAbsolutePath() + "/bg/dancefloor.png", 0.5, 0.5);
 		}
 
+		// Animation de tous les Instruments sauf le Piano lorsque la musique à déjà été jouée une fois
+		//Apparation du Texte précisant que l'on peut quitter la scène
 		if(alreadyPlay) {
 			animationDrum();
 			animationDoubleBass();
@@ -93,6 +143,7 @@ public class MenuVictory extends GameScreen {
 			StdDraw.setFont(new Font("sans serif",15,30));
 			StdDraw.text(0.5, 0.05,"Cliquez pour Quitter");
 		}
+		// Gestion du lancement des Animations de tous les Instruments sauf le Piano en fonction d'où en est la musique
 		else {
 			if(actualTime >= 1.142*4)
 				animationDoubleBass();
@@ -109,31 +160,45 @@ public class MenuVictory extends GameScreen {
 
 			StdDraw.picture(0.8, persoYPosition  -0.3, SpriteFilepath.getAbsolutePath() + "/mob/musical/drum_shadow.png", 0.2, 0.2);
 		}
-		
-		//affichage Danse
+
+		//Gestion de l'animation des Danseurs et du Piano à partir du moment où il y a de la musique
 		if( SoundPlayer.getBGMClip() != null) {
 			animationDanse();
 			animationPiano();
 		}
-		else
+		else {
+			// Affichage du dancefloor et les personnages 0.5s avant le démarrage
 			if(launchSong.getActualTime() <= 0.5 ) {
 				StdDraw.picture(0.5, persoYPosition - 0.18, SpriteFilepath.getAbsolutePath() + "/bg/dancefloor.png", 0.5, 0.5);
 				StdDraw.picture(0.4, persoYPosition, SpriteFilepath.getAbsolutePath() + "/mob/matthieu/matthieu_2.png", 0.2, 0.2);
 				StdDraw.picture(0.6, persoYPosition, SpriteFilepath.getAbsolutePath() + "/mob/julien/julien_2.png", 0.2, 0.2);
-				StdDraw.picture(0.5, persoYPosition  -0.2, SpriteFilepath.getAbsolutePath() + "/mob/musical/Piano_shadow.png", 0.2, 0.2);
 			}
 			else {
+				// Gestion de l'impression descente de la caméra
 				if(3-launchSong.getActualTime() >= 1) {
 					bgYPosition +=0.0085;
 				}
+				// AFfichage de l'ombre des danseurs et d'un effet assombrissant
 				StdDraw.picture(0.5, 0.5, SpriteFilepath.getAbsolutePath() + "/bg/Fondu.png",1,1);
 				StdDraw.picture(0.4, persoYPosition, SpriteFilepath.getAbsolutePath() + "/mob/matthieu/matthieu_shadow.png", 0.2, 0.2);
 				StdDraw.picture(0.6, persoYPosition, SpriteFilepath.getAbsolutePath() + "/mob/julien/julien_shadow.png", 0.2, 0.2);
-				StdDraw.picture(0.5, persoYPosition  -0.2, SpriteFilepath.getAbsolutePath() + "/mob/musical/Piano_shadow.png", 0.2, 0.2);
 			}
-
+			// Affichage de l'ombre du Piano tant que la musique n'a pas demarré
+			StdDraw.picture(0.5, persoYPosition  -0.2, SpriteFilepath.getAbsolutePath() + "/mob/musical/Piano_shadow.png", 0.2, 0.2);
+		}
 	}
 
+	//------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
+	/* 
+	**      GESTIONS DES ANIMATIONS (METHODES PRIVEES)
+	**      Toutes les Animations dépandent de actualTime.
+	 */
+	//------------------------------------------------------------------------------
+
+	/**
+	 * Gère l'animation des 2 Danseuses
+	 */
 	private void animationDanse() {
 		if((actualTime >= 1.142*3 && actualTime <= 1.142*4) || (actualTime >= 1.142*7 && actualTime <= 1.142*8) ||(actualTime >= 1.142*11 && actualTime <= 1.142*12) || (actualTime >= 1.142*15 && actualTime <= 1.142*16) ||(actualTime >= 1.142*19 && actualTime <= 1.142*20)) {
 			if(actualTime % 1.142 >= 0.8565) {
@@ -246,6 +311,9 @@ public class MenuVictory extends GameScreen {
 		}
 	}
 
+	/**
+	 * Gère l'animation du Piano
+	 */
 	private void animationPiano() {
 		if((actualTime >= 1.142*3 && actualTime <= 1.142*4) || (actualTime >= 1.142*7 && actualTime <= 1.142*8) ||(actualTime >= 1.142*11 && actualTime <= 1.142*12) || (actualTime >= 1.142*15 && actualTime <= 1.142*16) ||(actualTime >= 1.142*19 && actualTime <= 1.142*20)) {
 			if(actualTime % 1.142 >= 0.8565) {
@@ -278,6 +346,9 @@ public class MenuVictory extends GameScreen {
 		}
 	}
 
+	/**
+	 * Gère l'animation de la contre-basse
+	 */
 	private void animationDoubleBass() {
 		if((actualTime >= 1.142*3 && actualTime <= 1.142*4) || (actualTime >= 1.142*7 && actualTime <= 1.142*8) ||(actualTime >= 1.142*11 && actualTime <= 1.142*12) || (actualTime >= 1.142*15 && actualTime <= 1.142*16) ||(actualTime >= 1.142*19 && actualTime <= 1.142*20)) {
 			if(actualTime % 1.142 >= 0.8565) {
@@ -304,6 +375,9 @@ public class MenuVictory extends GameScreen {
 		}
 	}
 
+	/**
+	 * Gère l'animation du violon
+	 */
 	private void animationViolin() {
 		if(actualTime % 1.142 >= 0.8565) {
 			StdDraw.picture(0.7, persoYPosition - 0.25, SpriteFilepath.getAbsolutePath() + "/mob/musical/violin_2.png", 0.2, 0.2);
@@ -319,6 +393,9 @@ public class MenuVictory extends GameScreen {
 		}
 	}
 
+	/**
+	 * Gère l'animation de la guimbarde
+	 */
 	private void animationJewsharp() {
 		if((actualTime >= 1.142*3 && actualTime <= 1.142*4) || (actualTime >= 1.142*7 && actualTime <= 1.142*8) ||(actualTime >= 1.142*11 && actualTime <= 1.142*12) || (actualTime >= 1.142*15 && actualTime <= 1.142*16) ||(actualTime >= 1.142*19 && actualTime <= 1.142*20)) {
 			if(actualTime % 1.142 >= 0.8565) {
@@ -351,6 +428,9 @@ public class MenuVictory extends GameScreen {
 		}
 	}
 
+	/**
+	 * Gère l'animation de la batterie
+	 */
 	private void animationDrum() {
 		if((actualTime >= 1.142*3 && actualTime <= 1.142*4) || (actualTime >= 1.142*7 && actualTime <= 1.142*8) ||(actualTime >= 1.142*11 && actualTime <= 1.142*12) || (actualTime >= 1.142*15 && actualTime <= 1.142*16) ||(actualTime >= 1.142*19 && actualTime <= 1.142*20)) {
 			if(actualTime % 1.142 >= 0.8565) {
